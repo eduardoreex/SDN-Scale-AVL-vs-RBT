9e4ca235a9ee380605ee45447c1ec5a6be3a4093
Eduardo <eduardooliveira7907@gmail.com>
2026-05-20 22:43:44 -0300

    implementando Main com teste de insert, busca e delete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.avl.AVLRouterTree;
+import org.example.model.PacketRule;
+import org.example.rbt.RedBlackRouterTree;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        AVLRouterTree avl = new AVLRouterTree();
+        RedBlackRouterTree rbt = new RedBlackRouterTree();
+
+        System.out.println("===== INSERINDO REGRAS =====");
+
+        PacketRule[] regras = {
+                new PacketRule(1, "192.168.0.1", "10.0.0.1", 50),
+                new PacketRule(2, "192.168.0.2", "10.0.0.2", 30),
+                new PacketRule(3, "192.168.0.3", "10.0.0.3", 70),
+                new PacketRule(4, "192.168.0.4", "10.0.0.4", 20),
+                new PacketRule(5, "192.168.0.5", "10.0.0.5", 90),
+        };
+
+        for (PacketRule r : regras) {
+            avl.inserir(r);
+            rbt.inserir(r);
+            System.out.println("Inserida: " + r);
         }
+
+        System.out.println("\n===== ALTURAS =====");
+        System.out.println("Altura AVL: " + avl.getAltura());
+        System.out.println("Altura RBT: " + rbt.getAltura());
+
+
+        System.out.println("\n===== ROTAÇÕES =====");
+        System.out.println("Rotações AVL: " + avl.getTotalRotacoes());
+        System.out.println("Rotações RBT: " + rbt.getTotalRotacoes());
+
+        System.out.println("\n===== BUSCA (prioridade 70) =====");
+        System.out.println("AVL encontrou: " + avl.buscar(70));
+        System.out.println("RBT encontrou: " + rbt.buscar(70));
+
+        System.out.println("\n===== DELETANDO prioridade 30 =====");
+        avl.deletar(30);
+        System.out.println("AVL após deletar 30 → altura: " + avl.getAltura());
+        System.out.println("Busca por 30 na AVL: " + avl.buscar(30));
     }
 }
